532bf8c2297c416eb24880d849c40d99626815eb
Nazeli1 <167410271+Nazeli1@users.noreply.github.com>
2024-04-19 14:46:23 +0400

    Add files via upload

---
+++ commentoutthelines.cpp
@@ -0,0 +1,15 @@
+int main() {
+
+	std::cout << ”comment out me” << std::endl;
+	std::cout << ”comment out me” << std::endl;
+	std::cout << ”comment out me” << std::endl;
+	std::cout << ”comment out me” << std::endl;
+	std::cout << ”end” << std::endl;
+
+	return 0;
+}
+
+
+We need to press ctrl + V and select the line after that press esc, type I to enter insert mode after that just
+type // or /* in th beginning of the text and */ in the end of that text.
+
---
+++ findmatchingwordcountinaline.cpp
@@ -0,0 +1,8 @@
+Line  too line
+Line 2 foo
+Line foo
+Line foo foo
+Line 5 line
+
+We need to open file(example vim text.h), press esc and type ? foo.Type N to search backwards
+and n to search forwards, or just  type : % s / foo//gn
